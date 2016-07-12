aa8b247f3e2ec5fe2ec64050a0be3f88934ac571
Devian <forza9882@gmail.com>
2016-07-12 14:32:35 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+                <link type="text/css" rel="stylesheet" href="myStyleSheet.css">       
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js">
+        </script>
+     <style>
+        body {
+          color: #eeeeee;
+        }
+      </style>   
+    </head>
+    
+<body>
+
+  <div class="container-fluid">
+
+    <div class="row">
+      <div class="col-md-1 bg-info"><a href="https://github.com/deviank" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a></div>
+      <div class="col-md-1 bg-info"><a href="https://www.linkedin.com/in/devian-kapp-06490881" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></div>
+      <div class="col-md-1 bg-info"><a href="https://twitter.com/Devian82" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></div>
+      <div class="col-md-1 bg-info"><a href="mailto:kapp82@devian.co.za" target="_blank"><i class="fa fa-envelope" aria-hidden="true"></i></a></div>
+    </div>
+
+    <img class="img-circle img-responsive" src="http://www.devian.co.za/images/DK2.jpg" alt="profile_image" width="150px" height="120px" id="myImage">
+
+    <h1>Devian Kapp</h1>
+
+    <h4>Web Developer</h4>
+    <hr>
+    <br><br><br><br><br>
+
+    <div id="aboutMe">
+      <h4>About Me</h4>
+      <p>
+        My name is Devian a Cape Town(ZA) based Web Developer. I passionately use code to craft creative and user friendly web sites. My current arsenal consist of HTML, CSS, Bootstrap, jQuery and web development with PHP and MySQL with a dab of Javascript. Contact
+        me at your earliest convenience.I would love to hear about what you wish to achieve and to help you bring your ideas to life.
+      </p>
+      <hr>
+      <br><br><br><br>
+    </div>
+
+    <h4>Random Facts about myself(..not in any particular order)</h4>
+
+
+    <p> Love listening to music<br> Watching movies<br> Hardcore gamer<br> Learn other programming languages during my downtime</br>
+      Strive for world domination...one keystroke at a time<br>
+    </p>
+    <hr>
+
+    <br><br><br><br>
+
+
+    <h4>Programming Languages</h4>
+    <p>
+      HTML5<br> CSS3
+      <br> PHP and MySQL<br>
+    </p>
+    <hr>
+    <br><br><br><br>
+
+
+    <h4>Education</h4>
+    <p>
+      New Horizons Computer Learning Center<br> MasterGradeIT
+      <br>
+    </p>
+    <hr>
+    <br><br><br><br>
+    <h4>My Projects</h4>
+    <br><br>
+    <div class="row">
+      <div class="col-md-3">
+        <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSN4GjmLwQq9QJ6I1br11MOKHmhIq20nnAayFAGFFh8ZF6Rfh16qw" alt="projectImage_1">
+      </div>
+      <div class="col-md-3">
+        <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR9xeZ1yHLEA7IYGfP25LC6Oc0NAIJyZ3wCuXrwv_XohULAwCcW" alt="projectImage_2">
+      </div>
+      <div class="col-md-3">
+        <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTwbQUCqI38LzmXs7-WQgQ3Unhs2fV5COlWkT_DZgoNNr2n-GyP" alt="projImg_3">
+      </div>
+      <div class="col-md-3">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKaYPRF63ylJX8swZUIrf8qUH8Mw_u4utgtVKqGrZ7i7RmR2jXRg">
+      </div>
+    </div>
+    <hr>
+    <br><br>
+    <h4>Contact Me</h4>
+
+    <br>
+    <form role="form">
+      <div class="form-group">
+        <label for="firstname">Firstname:</label>
+        <input type=text " class="form-control " id="firstname " name="firstname " placeholder="Enter firstname ">
+        <label for="lastname ">Lastname:</label>
+        <input type="text " class="form-control " id="lastname " name="lastname " placeholder="Enter lastname ">
+        <label for="email ">Email:</label>
+        <input type="email " class="form-control " id="email " name="email " placeholder="Enter email ">
+        <br>
+        <label for="comment ">Comment:</label>
+        <textarea class="form-control " rows="5 " id="comment "></textarea><br>
+        <button type="submit " class="btn btn-default ">Submit</button>
+      </div>
+    </form>
+<hr>
+
+    <br><br><br>
+
+    <div class="row ">
+      <div class="col-md-1 bg-info "><a href="https://github.com/deviank " target="_blank "><i class="fa fa-github " aria-hidden="true "></i></a></div>
+      <div class="col-md-1 bg-info "><a href="https://www.linkedin.com/in/devian-kapp-06490881 " target="_blank "><i class="fa fa-linkedin " aria-hidden="true "></i></a></div>
+      <div class="col-md-1 bg-info "><a href="https://twitter.com/Devian82 " target="_blank "><i class="fa fa-twitter " aria-hidden="true "></i></a></div>
+      <div class="col-md-1 bg-info "><a href="mailto:kapp82@devian.co.za " target="_blank "><i class="fa fa-envelope " aria-hidden="true "></i></a></div>
+
+      <div id="mySignature ">
+        <p>&copy;Devian Kapp 2016</p>
+      </div>
+
+    </div>
+
+    <a href="# " class="go-top ">Back To Top</a>
+
+
+  </div>
+</body>
+</html>
